f1966ce756f6fa88a7fd72b7cb6013bbfaf07ac4
João Victor Perosso Souza <joaovperosso@gmail.com>
2025-10-30 21:49:31 -0300

    Day1
    I finished all the day 1.

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+//String and Integers
+
+var greeting = "Hello, playground"
+greeting = "Good bye"
+
+var age = 28
+var population = 8_000_000_000 //in swift '_' won't be read
+var population2 = 8000000000
+
+
+//Multiline strings
+
+//this keep with a \n between the words
+var multiLine = """
+this string
+has more
+then one 
+lines
+"""
+
+//Without the \n
+var noSpaces = """
+this string \
+has more \
+than one \
+lines
+"""
+
+//Doubles and Boooleans
+
+var pi = 3.14
+var isStudent = true
+
+//String interpolation
+
+var name = "John"
+var output = "Hello, how was your week \(name)?"
+
+//Constants
+
+let constant = "this is a constant, you can't change the value!"
+
+//Type annotations
+
+let myName: String = "John"
+var myage: Int = 28
+let haveDog: Bool = true
+let areUCrazy: Bool = true
+
+let money: Double = 2
+
+let dog: String
+dog = "Labrador"
+
+
